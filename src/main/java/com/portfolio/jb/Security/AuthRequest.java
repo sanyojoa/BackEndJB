d11668c7375af8d0b96671f49a5346eb0c372b3c
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-25 20:23:29 -0300

    Update Auth(Api/Request/Response)

--- src/main/java/com/portfolio/jb/Security/AuthRequest.java
+++ src/main/java/com/portfolio/jb/Security/AuthRequest.java
@@ -1,4 +1,29 @@
 package com.portfolio.jb.Security;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+
 public class AuthRequest {
+    @Email
+    @Length(min = 5, max = 50)
+    private String email;
+    @Length(min = 5, max = 50)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
