7df8e5afef695afc00cdbd7402d49fbee150dec7
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-01 20:36:53 -0300

    rol admin para crear editar y borrar

--- src/main/java/com/portfolio/jb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jb/Controller/PersonaController.java
@@ -3,6 +3,7 @@ package com.portfolio.jb.Controller;
 import com.portfolio.jb.Entity.Persona;
 import com.portfolio.jb.Interface.IPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,19 +19,21 @@ public class PersonaController {
         return iPersonaService.getPersona();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         iPersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         iPersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                               @RequestParam("nombre") String nuevoNombre,
