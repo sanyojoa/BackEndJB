bac74dbb21b096a4de4f5c827f5448f69cbae0c1
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-03 13:18:05 -0300

    Se creo educaicon entity

---
+++ src/main/java/com/portfolio/jb/Entity/Educacion.java
@@ -0,0 +1,47 @@
+package com.portfolio.jb.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Educacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreEd;
+    private String descripcionEd;
+
+    public Educacion() {
+    }
+
+    public Educacion(String nombreEd, String descripcionEd) {
+        this.nombreEd = nombreEd;
+        this.descripcionEd = descripcionEd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreEd() {
+        return nombreEd;
+    }
+
+    public void setNombreEd(String nombreEd) {
+        this.nombreEd = nombreEd;
+    }
+
+    public String getDescripcionEd() {
+        return descripcionEd;
+    }
+
+    public void setDescripcionEd(String descripcionEd) {
+        this.descripcionEd = descripcionEd;
+    }
+}
