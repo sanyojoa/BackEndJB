180034ed1c9b5ea03a8898d92448a4b36274bfcd
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-03 13:31:18 -0300

    Se creo dtoEducacion Dto

---
+++ src/main/java/com/portfolio/jb/Dto/dtoEducacion.java
@@ -0,0 +1,34 @@
+package com.portfolio.jb.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class dtoEducacion {
+    @NotBlank
+    private String nombreEd;
+    @NotBlank
+    private String descripcionEd;
+
+    public dtoEducacion() {
+    }
+
+    public dtoEducacion(String nombreEd, String descripcionEd) {
+        this.nombreEd = nombreEd;
+        this.descripcionEd = descripcionEd;
+    }
+
+    public String getNombreEd() {
+        return nombreEd;
+    }
+
+    public void setNombreEd(String nombreEd) {
+        this.nombreEd = nombreEd;
+    }
+
+    public String getDescripcionEd() {
+        return descripcionEd;
+    }
+
+    public void setDescripcionEd(String descripcionEd) {
+        this.descripcionEd = descripcionEd;
+    }
+}
