fb381ca35ee4f22c2d55c16492d35a6224b4de71
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-03 16:42:26 -0300

    Arreglado error de tipeo en SEducacion

--- src/main/java/com/portfolio/jb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/jb/Controller/CEducacion.java
@@ -72,7 +72,7 @@ public class CEducacion {
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
         }
-        if(sEducacion.existsByNombreE(dtoeducacion.getNombreE()) && sEducacion.getByNmbreE(dtoeducacion.getNombreE()).get().getId() != id){
+        if(sEducacion.existsByNombreE(dtoeducacion.getNombreE()) && sEducacion.getByNombreE(dtoeducacion.getNombreE()).get().getId() != id){
             return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
         }
         if(StringUtils.isBlank(dtoeducacion.getNombreE())){
