5a299872072ba3f5ae2a9a3226dc6626db48a6ca
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-04 17:41:48 -0300

    Se crep SProyecto Service

---
+++ src/main/java/com/portfolio/jb/Service/SProyecto.java
@@ -0,0 +1,45 @@
+package com.portfolio.jb.Service;
+
+import com.portfolio.jb.Entity.Proyecto;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+
+import com.portfolio.jb.Repository.RProyecto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class SProyecto {
+    @Autowired
+    RProyecto rProyecto;
+
+    public List<Proyecto> list(){
+        return rProyecto.findAll();
+    }
+
+    public Optional<Proyecto> getOne(int id){
+        return rProyecto.findById(id);
+    }
+
+    public Optional<Proyecto> getByNombreP(String nombreP){
+        return rProyecto.findByNombreE(nombreP);
+    }
+
+    public void save(Proyecto proyecto){
+        rProyecto.save(proyecto);
+    }
+
+    public void delete(int id){
+        rProyecto.deleteById(id);
+    }
+
+    public boolean existsById(int id){
+        return rProyecto.existsById(id);
+    }
+
+    public boolean existsByNombreP(String nombreP){
+        return rProyecto.existsByNombreP(nombreP);
+    }
+}
