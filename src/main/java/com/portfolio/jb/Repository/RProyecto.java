6e3bd504cc69ee50f3483b438d89538400bb6980
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-04 17:26:26 -0300

    Se creo RProyecto repository

---
+++ src/main/java/com/portfolio/jb/Repository/RProyecto.java
@@ -0,0 +1,13 @@
+package com.portfolio.jb.Repository;
+
+import java.util.Optional;
+
+import com.portfolio.jb.Entity.Proyecto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RProyecto extends JpaRepository<Proyecto, Integer>{
+    public Optional<Proyecto> findByNombreP(String nombreP);
+    public boolean existsByNombreP(String nombreP);
+}
