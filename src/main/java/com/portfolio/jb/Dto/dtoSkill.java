5cb7efc2fe8898b6103f1b8722c7431f5c19f408
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-08 12:51:27 -0300

    Se creo dtoSkill

---
+++ src/main/java/com/portfolio/jb/Dto/dtoSkill.java
@@ -0,0 +1,45 @@
+package com.portfolio.jb.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class dtoSkill {
+    @NotBlank
+    private String nombreS;
+    @NotBlank
+    private String porcentajeS;
+    @NotBlank
+    private String imgS;
+
+    public dtoSkill() {
+    }
+
+    public dtoSkill(String nombreS, String porcentajeS, String imgS) {
+        this.nombreS = nombreS;
+        this.porcentajeS = porcentajeS;
+        this.imgS = imgS;
+    }
+
+    public String getNombreS() {
+        return nombreS;
+    }
+
+    public void setNombreS(String nombreS) {
+        this.nombreS = nombreS;
+    }
+
+    public String getPorcentajeS() {
+        return porcentajeS;
+    }
+
+    public void setPorcentajeS(String porcentajeS) {
+        this.porcentajeS = porcentajeS;
+    }
+
+    public String getImgS() {
+        return imgS;
+    }
+
+    public void setImgS(String imgS) {
+        this.imgS = imgS;
+    }
+}
