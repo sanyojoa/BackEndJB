a3c1cafa91aa7d70ec34760320fe84560b1822b5
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-08 12:45:24 -0300

    Se creo Skill Entity

---
+++ src/main/java/com/portfolio/jb/Entity/Skill.java
@@ -0,0 +1,57 @@
+package com.portfolio.jb.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreS;
+    private String porcentajeS;
+    private String imgS;
+
+    public Skill() {
+    }
+
+    public Skill(String nombreS, String porcentajeS, String imgS) {
+        this.nombreS = nombreS;
+        this.porcentajeS = porcentajeS;
+        this.imgS = imgS;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreS() {
+        return nombreS;
+    }
+
+    public void setNombreS(String nombreS) {
+        this.nombreS = nombreS;
+    }
+
+    public String getPorcentajeS() {
+        return porcentajeS;
+    }
+
+    public void setPorcentajeS(String porcentajeS) {
+        this.porcentajeS = porcentajeS;
+    }
+
+    public String getImgS() {
+        return imgS;
+    }
+
+    public void setImgS(String imgS) {
+        this.imgS = imgS;
+    }
+}
